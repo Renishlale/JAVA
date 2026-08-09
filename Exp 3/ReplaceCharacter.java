231f584a1ce748b2b0be3412697bcd1ee3249a81
Renishlale <lalerenishcode@gmail.com>
2026-08-09 22:42:27 +0530

    Update ReplaceCharacter.java

--- Exp 3/ReplaceCharacter.java
+++ Exp 3/ReplaceCharacter.java
@@ -1,12 +1,14 @@
+```java
 class ReplaceCharacter
 {
     public static void main(String args[])
     {
         String str = "Java";
 
-        String newString = str.substring(0, 2) + "v" + str.substring(3);
+        String newString = str.substring(0, 2) + "w" + str.substring(3);
 
         System.out.println("Original String: " + str);
         System.out.println("New String: " + newString);
     }
 }
+```
